7ee59bbcde867f0ffb3e1aad9f3da995d8fc2873
Don Hamilton III <donhamiltoniii@gmail.com>
2019-10-11 14:24:52 -0400

    Finish workshop

--- src/controllers/cohort-controller.js
+++ src/controllers/cohort-controller.js
@@ -2,18 +2,18 @@ const cohortService = require('../services/cohort-service')
 const Cohort = require('../models/Cohort')
 
 class CohortController {
+  static async addCohort (req, res) {
+    const language = req.body.language
+    await cohortService.save(new Cohort(language))
+    res.redirect('/cohorts')
+  }
   static async renderAll (req, res) {
     res.render("cohorts/all", { cohorts: await cohortService.findAll() })
   }
   static async renderSingle (req, res) {
     const cohort = await cohortService.findById(req.params.id)
-    const students = await cohort.getStudents()
-    res.render("cohorts/single", { cohort, students })
-  }
-  static async addCohort (req, res) {
-    const language = req.body.language
-    await cohortService.save(new Cohort(language))
-    res.redirect('/cohorts')
+    cohort.students = await cohort.getStudents()
+    res.render("cohorts/single", { cohort })
   }
 }
 
